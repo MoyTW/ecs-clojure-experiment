be60a43db0c736ccd9994e755b48d8fd2b2b104b
MoyTW <moytravis@gmail.com>
2016-05-03 18:59:48 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ecs-experiment
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ecs_experiment/core_test.clj
@@ -0,0 +1,7 @@
+(ns ecs-experiment.core-test
+  (:require [clojure.test :refer :all]
+            [ecs-experiment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
