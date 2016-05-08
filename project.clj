a61a5c27ac743570ecdb93e810022a53504fe29a
MoyTW <moytravis@gmail.com>
2016-05-08 16:51:18 -0700

    Add quil
    Never used it before but it seems nifty.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.1"]]
+                 [prismatic/schema "1.1.1"]
+                 [quil "2.4.0"]]
   :main ^:skip-aot ecs-experiment.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ecs_experiment/core.clj
+++ src/ecs_experiment/core.clj
@@ -1,7 +1,28 @@
 (ns ecs-experiment.core
-  (:gen-class))
+  (:require [quil.core :as q]))
+
+(defn setup []
+  (q/frame-rate 30)
+  (q/background 200))
+
+(defn draw-random-circles []
+  (q/stroke (q/random 255) (q/random 50) (q/random 255))
+  (q/stroke-weight (q/random 10))
+  (q/fill (q/random 255))
+
+  (let [diam (q/random 100)
+        x (q/random (q/width))
+        y (q/random (q/height))]
+    (q/ellipse x y diam diam)))
+
+(defn example-window []
+  (q/defsketch example
+    :title "Random Red/Purple Circles"
+    :setup setup
+    :draw draw-random-circles
+    :size [640 480]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (example-window))
