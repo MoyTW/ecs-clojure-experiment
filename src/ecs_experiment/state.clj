77c068b7b6595bb97ffe08b3fe3ae85599bd3f48
MoyTW <moytravis@gmail.com>
2016-05-10 20:53:05 -0700

    Add entity-generation/adding functions
    Also retrofitted test to use them. It's a little awkward.

--- src/ecs_experiment/state.clj
+++ src/ecs_experiment/state.clj
@@ -1,5 +1,6 @@
 (ns ecs-experiment.state
   (:require [ecs-experiment.components.schema :as cs]
+            [ecs-experiment.utils :as utils]
             [schema.core :as s]))
 
 ;; The map nesting will get extremely silly. You might want to consider Specter
@@ -15,9 +16,35 @@
    {:keys [component-key] :as component} :- cs/Component]
   (assoc-in entity [:components component-key] component))
 
+(s/defn create-entity :- Entity
+  [components :- [cs/Component]]
+  (let [entity {:entity-id (utils/gen-uuid)
+                :components {}}]
+    (reduce assoc-component entity components)))
+
 (def State
   {:entities {s/Str Entity}
    :systems [s/Keyword]})
+
+(s/defn create-empty-state :- State
+  []
+  {:entities {},
+   :systems []})
+
+(s/defn get-entity-ids :- [s/Str]
+  [state :- State]
+  (keys (:entities state)))
+
+(s/defn assoc-entity :- State
+  [state :- State
+   {:keys [entity-id] :as entity} :- Entity]
+  (assoc-in state [:entities entity-id] entity))
+
+(s/defn create-and-assoc-entity :- State
+  [state :- State
+   components :- [cs/Component]]
+  (let [{:keys [entity-id] :as entity} (create-entity components)]
+    (assoc-entity state entity)))
 
 (s/defn get-component-by-entity-id :- cs/Component
   [state :- State
--- test/ecs_experiment/systems/position_test.clj
+++ test/ecs_experiment/systems/position_test.clj
@@ -5,19 +5,13 @@
             [ecs-experiment.state :as state]
             [ecs-experiment.systems.position :as position-s]))
 
-(def entity-id "test-entity")
-
-(def base-state
-  {:entities {entity-id {:entity-id entity-id,
-                         :components {}}},
-   :systems []})
-
 (def test-state
-  (-> base-state
-      (state/assoc-component-by-entity-id
-       entity-id (velocity-c/create-velocity-component 3 1))
-      (state/assoc-component-by-entity-id
-       entity-id (position-c/create-position-component 10 10))))
+  (-> (state/create-empty-state)
+      (state/create-and-assoc-entity
+       [(velocity-c/create-velocity-component 3 1)
+        (position-c/create-position-component 10 10)])))
+
+(def entity-id (first (state/get-entity-ids test-state)))
 
 (deftest sums-velocity-and-position
   (let [position-system (position-s/create-position-system nil)
